f16a4a9b694af114558dbe2fb871ae08d2435bae
Yashil Vora <yvora@ucsd.edu>
2024-05-18 12:29:55 -0700

    expose done

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,10 @@ function getRecipesFromStorage() {
 	// A9. TODO - Complete the functionality as described in this function
 	//           header. It is possible in only a single line, but should
 	//           be no more than a few lines.
+	const recipes = localStorage.getItem("recipes");
+	if (!recipes) return [];
+	else return JSON.parse(recipes);
+
 }
 
 /**
@@ -35,10 +39,19 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
 	// A10. TODO - Get a reference to the <main> element
+	const main = document.querySelector('main');
 	// A11. TODO - Loop through each of the recipes in the passed in array,
 	//            create a <recipe-card> element for each one, and populate
 	//            each <recipe-card> with that recipe data using element.data = ...
 	//            Append each element to <main>
+	for (let i = 0; i < recipes.length; i++) {
+		// <recipe-card>
+		const recipeCard = document.createElement('recipe-card');
+		// populating <recipe-card>
+		recipeCard.data = recipes[i];
+		// appending to main
+		main.append(recipeCard);
+	}
 }
 
 /**
